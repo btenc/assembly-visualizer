8b59be80c49a08b01cf40c04083dda22c4905c39
William Ten Cate <billytencate@gmail.com>
2024-11-24 14:31:18 -0500

    Begin basic arithmetic instruction implementation

---
+++ src/modules/asm/asmHelpers.js
@@ -0,0 +1,64 @@
+function validateStrToNum(strNum) {
+  if (strNum === undefined || strNum === null) {
+    throw "Error: parameter must be supplied";
+  }
+
+  if (typeof strNum !== "string") {
+    throw "Error: Must be of type string to be converted to Number.";
+  }
+
+  strNum = strNum.trim();
+  if (strNum === "") {
+    throw "Error: string cannot be empty or only spaces";
+  }
+
+  const convertedToNum = Number(strNum);
+  if (isNaN(convertedToNum)) {
+    throw "Error: Supplied string is not a number";
+  }
+  if (!Number.isInteger(convertedToNum)) {
+    throw "Error: supplied string is not an integer.";
+  }
+
+  return convertedToNum;
+}
+
+function isValidNumberHelper(value) {
+  try {
+    validateStrToNum(value);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function argsCheck(registers, args) {
+  if (args.length !== 2) {
+    throw "Error: there must only be two arguments";
+  }
+  if (!(args[0] in registers)) {
+    throw "Error: " + args[0] + " is not a valid register";
+  }
+  if (!isValidNumberHelper(args[1]) && !(args[1] in registers)) {
+    throw "Error: " + args[1] + " is not a valid register or a valid number";
+  }
+}
+
+function isSecondArgNumber(registers, arg) {
+  if (isValidNumberHelper(arg)) {
+    return true;
+  }
+  if (arg in registers) {
+    return false;
+  } else {
+    throw "Error: " + arg + " is not a number or register";
+  }
+}
+
+const asmHelpers = {
+  validateStrToNum,
+  argsCheck,
+  isSecondArgNumber,
+};
+
+export default asmHelpers;
---
+++ src/modules/asm/instructions.js
@@ -0,0 +1,46 @@
+/* 
+Many instructions based on a stripped down form of 6502 assembly... 
+Key Reference:
+http://www.6502.org/users/obelisk/6502/reference.html
+*/
+
+// Note: REGISTERS is a class with data of JS Object
+/* 
+ARGS will be array of length 2, 
+first argument is register name, 
+second is the argument value (can be number or register depending on the instruction).
+*/
+
+import asmHelpers from "./asmHelpers";
+
+//____ARITHMETIC FUNCTIONS____
+
+function ADD(registers, args) {
+  asmHelpers.argsCheck(args);
+
+  const secondArgNum = asmHelpers.isSecondArgNumber(args[1]);
+  if (secondArgNum === true) {
+    return; //todo
+  }
+  if (secondArgNum === false) {
+    return; //todo
+  } else {
+    throw "Error: Something went really wrong in the ADD function!";
+  }
+}
+
+function SUB(registers, args) {}
+
+function MUL(registers, args) {}
+
+function DIV(registers, args) {}
+
+function INC(registers, args) {}
+
+function DEC(registers, args) {}
+
+// ____BITWISE FUNCTIONS____
+
+// ____MOVEMENT FUNCTIONS____
+
+// ____CONTROL FUNCTIONS____
