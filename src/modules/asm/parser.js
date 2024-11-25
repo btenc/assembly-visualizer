45432b24f5240e9bd9f8938f2b213f96aaa46893
William Ten Cate <billytencate@gmail.com>
2024-11-25 13:33:50 -0500

    Add base register class functionality

---
+++ src/modules/asm/parser.js
@@ -0,0 +1,11 @@
+/*
+const program = [
+  { instruction: 'MOV', args: ['R1', '5'] },
+  { instruction: 'ADD', args: ['R1', '3'] },
+  { instruction: 'JMP', args: ['0'] }, 
+];
+*/
+
+//program.length is how long the program is
+//this should parse out the above array from strings
+//doing it this way makes it easy to use the IP as an index for what instruction we are on
