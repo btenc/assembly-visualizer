7116447bd459d88154e962177faae6e5cd4402dd
William Ten Cate <billytencate@gmail.com>
2024-11-29 12:21:03 -0500

    Add first case to instruction router

--- src/modules/asm/instructionRouter.js
+++ src/modules/asm/instructionRouter.js
@@ -1,4 +1,4 @@
-//todo -> switch statements to route to correct instruction
+import asmInstructions from "./instructions.js";
 function routeInstruction(
   AsmInterpreterService,
   currentStatement,
@@ -6,4 +6,12 @@ function routeInstruction(
 ) {
   const instruction = currentStatement.statement;
   const args = currentStatement.arguments;
+
+  // for the jump cases, make sure it's a valid place to jump to before I send it over. that's why program length is passed in
+  switch (instruction) {
+    case "ADD":
+      asmInstructions.ADD(AsmInterpreterService.registers, args);
+    default:
+      throw "Error: " + instruction + " is not a supported instruction!";
+  }
 }
