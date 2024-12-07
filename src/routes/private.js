5b4fce4b4618d3cd78a79c1b08855c69c666ad54
Woodsii <beanmanwoods@gmail.com>
2024-12-07 15:59:29 -0500

    Finish session implimentation

--- src/routes/private.js
+++ src/routes/private.js
@@ -1,6 +1,22 @@
 import { Router } from "express";
 const router = Router();
 
-//todo
+/* How to use: 
+
+    For any function that requires authentication, it should follow this format: 
+
+    app.get('/route', requireAuth, (req, res) => {
+        // do authenticated shit.
+    });
+
+    Where the requireAuth is a middleware that runs before the route is executed.
+*/
+const requireAuth = (req, res, next) => {
+    if (req.session.userId) {
+        next(); // User is authenticated, continue to next middleware
+    } else {
+        res.redirect('/login'); // User is not authenticated, redirect to login page
+    }
+}
 
 export default router;
