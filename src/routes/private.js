2195da6c4d9caffdef6ec8eac8f99d317c173a72
Woodsii <beanmanwoods@gmail.com>
2024-12-08 14:28:50 -0500

    Add xss lib, flesh out private routes

--- src/routes/private.js
+++ src/routes/private.js
@@ -19,4 +19,19 @@ const requireAuth = (req, res, next) => {
     }
 }
 
+// will get private information abt the user
+router.get('/', requireAuth, (req, res) => {
+
+});
+
+// updates user information
+router.post('/', requireAuth, (req, res) => {
+
+});
+
+// will load the user's dashboard
+router.get('/:userId', requireAuth, (req, res) => {
+    
+});
+
 export default router;
