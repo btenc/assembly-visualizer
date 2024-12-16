a342673eba87a1e00a3d553b44d054a962075c44
Jorge-2104 <jorgeiiiponce210@gmail.com>
2024-12-16 17:11:38 -0500

    if we look for invalid snippet navBar should still show up

--- src/routes/snippets.js
+++ src/routes/snippets.js
@@ -112,7 +112,7 @@ router
     } catch (e) {
       // Even in the error case, we can pass isLoggedIn so the navbar doesn't break
       res.render("pages/snippets", {
-        isLoggedIn: isLoggedIn,
+        isLoggedIn: !req.session.username ? false : true,
         isOwner: false,
         username: req.session.username,
         errors: [e],
@@ -176,16 +176,20 @@ router
   })
   .delete(async (req, res) => {
     if (!req.params.snippetID)
-      return res.status(400).render("pages/snippets", { errors: ["400, snippetID not found"] });
-    
+      return res
+        .status(400)
+        .render("pages/snippets", { errors: ["400, snippetID not found"] });
+
     try {
       let removed = await snippetMethods.removeSnippet(req.params.snippetID);
 
-      return res.status(200).json({message: "Snippet deleted successfully :^)"});
+      return res
+        .status(200)
+        .json({ message: "Snippet deleted successfully :^)" });
     } catch (e) {
       console.error("Error in deleting a snippet: ", e);
 
-      return res.status(400).json({errors: [e]})
+      return res.status(400).json({ errors: [e] });
     }
   });
 
