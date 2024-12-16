ba0a595407969f72ba3b5ff635f951f5b013f54b
smGarc <sgarcia4@stevens.edu>
2024-12-16 13:47:26 -0500

    Add featured snippets to homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,27 @@
 import snippetRoutes from "./snippets.js";
 import userRoutes from "./users.js";
+import snippetMethods from '../data/snippets.js';
+import userMethods from '../data/users.js';
 
 const constructorMethod = (app) => {
   // hompage <3
   // Impliment logic for different views if the user is signed in or not.
-  app.use("/homepage", (req, res) => {
-    const loggedOut = req.query.loggedOut === "true";
+  app.use("/homepage", async (req, res) => {
     try {
-      res.render("pages/home", { loggedOut });
+      let snippets = [];
+      snippets = await snippetMethods.getAllSnippets();
+      for (var snip in snippets) {
+        let user = await userMethods.getUserById(snippets[snip].userId);
+        snippets[snip] = {
+          _id: snippets[snip]._id,
+          snipName: snippets[snip].snipName,
+          snipBody: snippets[snip].snipBody,
+          username: user.username,
+          dateCreation: snippets[snip].dateCreation,
+          dateLastEdit: snippets[snip].dateLastEdit
+        }
+      }
+      res.render("pages/home", {snippets: snippets});
     } catch (e) {
       return res.status(500).json(e);
     }
